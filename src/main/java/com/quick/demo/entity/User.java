0f4e8edb4d8463eb3ff61aa713f14f6b89d137b6
liangbao.wang <liangbao.wang@derbysoft.com>
2021-11-26 19:29:00 +0800

    specify column name in camel case style

--- src/main/java/com/quick/demo/entity/User.java
+++ src/main/java/com/quick/demo/entity/User.java
@@ -3,19 +3,67 @@ package com.quick.demo.entity;
 import javax.persistence.*;
 
 @Entity
-@Table(indexes = @Index(columnList = "userId"))
+@Table(indexes = @Index(columnList = "user_id"))
 public class User {
 
 	@Id()
 	@Column()
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	@Column(unique = true)
+	@Column(name = "user_id", unique = true)
 	private String userId;
 	private String name;
 	private String dob;
 	@Embedded
 	private Address address;
+	@Column(name = "created_at")
 	private long createdAt;
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public long getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(long createdAt) {
+		this.createdAt = createdAt;
+	}
 }
