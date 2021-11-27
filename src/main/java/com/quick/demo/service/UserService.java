978802fde84b0adcae53004fb0569e4b8e6f4d93
liangbao.wang <liangbao.wang@derbysoft.com>
2021-11-27 15:52:24 +0800

    add use geo when save user /update use

--- src/main/java/com/quick/demo/service/UserService.java
+++ src/main/java/com/quick/demo/service/UserService.java
@@ -1,19 +1,27 @@
 package com.quick.demo.service;
 
 import com.quick.demo.dto.UserDTO;
+import com.quick.demo.entity.Address;
 import com.quick.demo.entity.User;
 import com.quick.demo.exception.UserNotFoundException;
+import com.quick.demo.repository.UserRelationRepository;
 import com.quick.demo.repository.UserRepository;
 import com.quick.demo.util.Current;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private UserRelationRepository relationRepository;
 
 	public UserDTO getByUserNo(String userNo) {
 		return toDTO(userRepository.getByUserNo(userNo));
@@ -25,6 +33,7 @@ public class UserService {
 		user.setDob(userDTO.getDob());
 		user.setName(userDTO.getName());
 		user.setCreatedAt(Current.toMills());
+		addGeo(user);
 		return toDTO(userRepository.saveOrUpdate(user));
 
 	}
@@ -39,6 +48,7 @@ public class UserService {
 		user.setName(userDTO.getName());
 		user.setDob(userDTO.getDob());
 		user.setAddress(userDTO.getAddress());
+		addGeo(user);
 		return toDTO(userRepository.saveOrUpdate(user));
 
 	}
@@ -62,5 +72,14 @@ public class UserService {
 		userDTO.setAddress(user.getAddress());
 		userDTO.setCreateAt(user.getCreatedAt());
 		return userDTO;
+	}
+
+	private void addGeo(User user) {
+		String latitude = Optional.of(user).map(User::getAddress).map(Address::getLatitude).orElse(null);
+		String longitude = Optional.of(user).map(User::getAddress).map(Address::getLongitude).orElse(null);
+		if (StringUtils.isBlank(latitude) || StringUtils.isBlank(longitude)) {
+			return;
+		}
+		relationRepository.addGeo(user.getUserNo(), user.getAddress());
 	}
 }
